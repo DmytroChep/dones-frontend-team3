31ea3ff9e253dff89471de7d615cb481b137f88c
DmytroChep <dmitriychep2011@gmail.com>
2026-01-15 20:35:03 +0200

    create main but now without adaptive

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -11,14 +11,27 @@ export function About() {
                 <div className={styles.aboutUsText}>
                     <p className={styles.adoutUsTextHeader}>ПРО НАС</p>
                     <p className={styles.adoutUsTextMain}>Ми — команда, яка об'єднана спільною метою: зробити передові технології 
-                        доступними для кожного, хто потребує точності, безпеки та інновацій. З 2022 року ми спеціалізуємось на 
+                        доступними для кожного, хто потребує точності, безпеки та інновацій. <br/>З 2022 року ми спеціалізуємось на 
                         постачанні дронів і тепловізорів для професійного, цивільного та волонтерського використання.
                     </p>
-                    <img src={IMAGES.aboutImage1} className={styles.imageBLock1}/>
                 </div>
+                <img src={IMAGES.aboutImage1} className={styles.imageBlock1}/>
             </div>
             <div className={styles.secondAndThirdBlock}>
-                <div></div>
+                <div className={styles.smallBlocksText}>
+                    <p className={styles.smallBlocksTextHeader}>НАША МІСІЯ</p>
+                    <p className={styles.smallBlocksTextMain}>Допомагати тим, хто стоїть на передовій — у прямому й переносному сенсі.
+                        <br/> Ми обираємо тільки надійну техніку, яку перевіряємо самі. Наша мета — якість, простота, і підтримка на 
+                        кожному етапі: від покупки до використання.</p>
+                </div>
+                <img src={IMAGES.aboutImage2} className={styles.imageBlock2}/>
+            </div>
+                <div className={styles.secondAndThirdBlock}>
+                <img src={IMAGES.aboutImage3} className={styles.imageBlock2}/>
+                <div className={styles.smallBlocksText}>
+                    <p className={styles.smallBlocksTextHeader}>КОМАНДА, ЯКИЙ МОЖНА ДОВІРЯТИ</p>
+                    <p className={styles.smallBlocksTextMain}>Ми — не просто магазин. Ми — фахівці, які самі працюють із цією технікою й консультують з досвіду. Засновники проєкту — волонтери, військові та IT-спеціалісти, які об'єднали зусилля задля важливої справи.</p>
+                </div>
             </div>
         </div>
     )
